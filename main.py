7ec89d90b3ef818873a1afcc0b9e0efdaf6b4110
sofiaperiv <151615889+sofiaperiv@users.noreply.github.com>
2023-12-02 22:38:45 +0200

    python file, kivy file, additional images added

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.behaviors import FakeRectangularElevationBehavior
 from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.screen import MDScreen
+from kivymd.uix.screen import Screen
+from kivy.graphics import Color, Ellipse
+from kivymd.uix.card import MDCard
 from kivy.graphics import Rectangle
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_file("openscreen.kv")
 
-
-class OpenScreen(MDScreen):
+class MainScreen(Screen):
     def ask_permission(self):
         ask_permission = self.ids.ask_permission
         permission = self.ids.permission
@@ -41,10 +42,22 @@ class MyRectangle(MDFloatLayout):
     pass
 
 
-class MyApp(MDApp):
-    def build(self):
+class HomeScreen(Screen):
+    pass
 
-        return OpenScreen()
+
+class WindowManager(ScreenManager):
+    pass
+
+
+
+
+
+class MyApp(MDApp):
+
+    def build(self):
+        kv = Builder.load_file("mainScreen.kv")
+        return kv
 
     def change_color(self, instance):
         if instance in self.root.ids.values():
@@ -54,6 +67,7 @@ class MyApp(MDApp):
                     self.root.ids[f"nav_icon{i + 1}"].text_color = 1, 0, 0, 1
                 else:
                     self.root.ids[f"nav_icon{i + 1}"].text_color = 0, 0, 0, 1
+
 
 
 if __name__ == "__main__":
